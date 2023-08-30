f06110d5a9fbf28220ca452900e64f2ca3eeeaf6
JatoMixo <jatomixo@outlook.es>
2023-08-30 16:01:23 +0200

    Sort of configured LSP, still need to do some changes

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,7 +3,19 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"rust_analyzer",
+  'rust_analyzer',
+})
+
+lsp.setup_servers({'rust_analyzer'})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
 })
 
 lsp.setup()
