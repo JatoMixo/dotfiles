ea1bd7427edb1be16ef3a31976c57ab419e92a47
JatoMixo <jatomixo@outlook.es>
2023-08-31 12:15:47 +0200

    LSP Successfully setup, still need to install rust-analyzer

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,21 +1,14 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
+lsp.preset('recommended')
 
 lsp.ensure_installed({
   'rust_analyzer',
 })
 
-lsp.setup_servers({'rust_analyzer'})
-
 lsp.set_preferences({
     suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+    sign_icons = { }
 })
 
 lsp.setup()
