87204a23a17c0ec1f34a7cbf13d04c1a319a89b1
JatoMixo <jatomixo@proton.me>
2024-02-10 12:28:37 +0100

    Small change to nvim lsp config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -15,7 +15,25 @@ lsp.ensure_installed({
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
-    sign_icons = { }
+    sign_icons = {
+        error = ' ',
+        warn = ' ',
+        hint = '󰌵',
+        info = ' ',
+    },
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
 })
 
 lsp.setup()
